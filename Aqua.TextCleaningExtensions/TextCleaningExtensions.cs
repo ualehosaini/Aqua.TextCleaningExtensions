3db4d170ef789b3955fa97a8359d09c3116f2dff
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-01-08 21:57:33 +0000

    added IsNullOrEmpty

---
+++ Aqua.TextCleaningExtensions/TextCleaningExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aqua.TextCleaningExtensions
+{
+    public static class TextCleaningExtensions
+    {
+        /// <summary>
+        /// Is Null Or Empty String?
+        /// </summary>
+        /// <param name="s">string</param>
+        /// <returns>true/false</returns>
+        public static bool IsNullOrEmpty(this string s) => string.IsNullOrEmpty(s);
+
+    }
+}
