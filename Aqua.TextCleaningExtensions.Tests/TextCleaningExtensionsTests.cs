be1e1f12db2004c5c90988bbc5ad02f461869635
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-27 17:32:33 +0000

    InNullOrEmpty_Valid has been added

--- Aqua.TextCleaningExtensions.Tests/TextCleaningExtensionsTests.cs
+++ Aqua.TextCleaningExtensions.Tests/TextCleaningExtensionsTests.cs
@@ -1,7 +1,15 @@
+using Xunit;
+
 namespace Aqua.TextCleaningExtensions.Tests
 {
     public class TextCleaningExtensionsTests
     {
-
+        [Theory]
+        [InlineData("", true)]
+        [InlineData(" ", false)]
+        [InlineData(null, true)]
+        [InlineData("lorem ipsum dolor", false)]
+        public void InNullOrEmpty_Valid(string input, bool expected)
+            => Assert.Equal(expected, input.IsNullOrEmpty());
     }
 }
