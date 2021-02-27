573d8a801fb15a3d684d20691b9d71e0281a53dd
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-27 17:33:10 +0000

    InNullOrWhiteSpace_Valid has been added

--- Aqua.TextCleaningExtensions.Tests/TextCleaningExtensionsTests.cs
+++ Aqua.TextCleaningExtensions.Tests/TextCleaningExtensionsTests.cs
@@ -11,5 +11,13 @@ namespace Aqua.TextCleaningExtensions.Tests
         [InlineData("lorem ipsum dolor", false)]
         public void InNullOrEmpty_Valid(string input, bool expected)
             => Assert.Equal(expected, input.IsNullOrEmpty());
+
+        [Theory]
+        [InlineData("", true)]
+        [InlineData(" ", true)]
+        [InlineData(null, true)]
+        [InlineData("lorem ipsum dolor", false)]
+        public void InNullOrWhiteSpace_Valid(string input, bool expected)
+            => Assert.Equal(expected, input.IsNullOrWhiteSpace());
     }
 }
