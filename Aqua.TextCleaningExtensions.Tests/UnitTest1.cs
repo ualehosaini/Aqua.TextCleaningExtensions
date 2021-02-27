1024beb38e3ca7f1f582015e8768290673ef27fb
ualehosaini2019 <ualehosaini2019@hotmail.com>
2021-02-27 17:24:47 +0000

    Added test project

---
+++ Aqua.TextCleaningExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Aqua.TextCleaningExtensions.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
